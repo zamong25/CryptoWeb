d624f120e7fb3f286dc51b0ab86e817c4b5a9968
uzzing <jungeunhart@gmail.com>
2021-08-31 02:35:08 +0900

    2021.08.31 2:31AM
    [What we did]
    1. make data to json file
    2. send data from .ejs to express(.js) by ajax
    3. send json data from express to .ejs and get, retrieve specific value
    
    [ To do list ]
    1. add goodName and details to lotteMart.json, lotteSuper.json, lotteDepartment.json by using excel
    2. make the popup and retrieve real datas

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,7 @@ const DomParser = require("dom-parser");
 const parser = new DomParser();
 const convert = require('xml-js');
 const request = require('request');
+const { getSystemErrorMap } = require('util');
 
 process.on("uncaughtException", function(err) { 
   console.error("uncaughtException (Node is alive)", err); 
@@ -23,46 +24,41 @@ router.get('/', function(req, res) {
   fs.readFile("./martinfo.json", 'utf8', function (err, data) {
     var mart = JSON.parse(data);
     res.render("index.ejs", { mart : mart });
-    for (var i = 0; i < 1; i++) {
-
-      let options = {
-        method: "GET",
-        hostname: "openapi.price.go.kr",
-        path: "/openApiImpl/ProductPriceInfoService/getProductPriceInfoSvc.do",
-        params: {
-          goodInspectDay : '20210820',
-          entpId : mart.list[i].entpId,
-          ServiceKey : 'yNc8/uCtg/QQnPAv9+JOrvKD4SIFKlNW/sHUVUzDwV55Q8Qz1UsPFApX32Y/WXM9xRdqHxatmGvLf81uPv6ulQ=='
-        }
-      }
-
-      let url = "http://openapi.price.go.kr/openApiImpl/ProductPriceInfoService/getProductPriceInfoSvc.do?" + new URLSearchParams({
-        goodInspectDay : '20210820',
-        entpId : mart.list[i].entpId,
-        ServiceKey : 'yNc8/uCtg/QQnPAv9+JOrvKD4SIFKlNW/sHUVUzDwV55Q8Qz1UsPFApX32Y/WXM9xRdqHxatmGvLf81uPv6ulQ=='
-      });
-
-      let URL = "http://openapi.price.go.kr/openApiImpl/ProductPriceInfoService/getProductPriceInfoSvc.do?goodInspectDay=20210820&entpId=642&ServiceKey=yNc8%2FuCtg%2FQQnPAv9%2BJOrvKD4SIFKlNW%2FsHUVUzDwV55Q8Qz1UsPFApX32Y%2FWXM9xRdqHxatmGvLf81uPv6ulQ%3D%3D";
-
-      request.get(URL, (err, res, body)=> {
-        if (err) {
-            console.log(`err => ${err}`);
-        }
-        else {
-            if (res.statusCode == 200) {
-              var result = body;
-              // console.log(`body data => ${result}`);
-              var xmlToJson = convert.xml2json(result, {compact: true, spaces: 4});
-              // console.log(`xml to json => ${xmlToJson}`);
-              fs.writeFile('./productPriceInfo.json', xmlToJson, 'utf8', () => {
-                console.log("done");
-              });
-            }
-        }
-      });
-    }
   });
 });
+
+router.post('/', function(req, res, next) {
+
+  var martName = req.body.martName;
+  console.log(martName);
+
+  if (martName.includes("롯데마트")) {
+    console.log("롯데마트 파일 가져와");
+    var data = fs.readFileSync('./lotteMart.json', 'utf8');
+    console.log(data);
+    res.render("index.ejs", { products : data });
+  }
+  else if (martName.includes("롯데슈퍼")) {
+    console.log("롯데슈퍼 파일 가져와");
+    var data = fs.readFileSync('./lotteSuper.json', 'utf8');
+    //console.log(data);
+    res.json(JSON.parse(data));
+  }
+  else if (martName.includes("롯데백화점")) {
+    console.log("롯데백화점 파일 가져와");
+    var data = fs.readFileSync('./lotteDepartment.json', 'utf8');
+    console.log(data);
+    res.render("index.ejs", { products : data });
+  }
+  else {
+    console.log("아무것도 아니야");
+    var data = fs.readFileSync('./lotteSuper.json', 'utf8');
+    console.log(data);
+    res.render("index.ejs", { products : data });
+  }
+  
+});
+
 
 router.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -74,6 +70,38 @@ router.use(express.static("public"));
 
 module.exports = router;
 
+/* 
+1. get mart.list[i].name from index.ejs to index.js -> router.get(url, function (req, res) {
+    let name = req.
+})
+2. send products data by reading json file from index.js to index.ejs -> res.render()
+*/
+/*
+if (mart.list[i].name.contains("롯데마트")) {
+  fs.readFile("./lotteMart.json", 'utf8', function (err, data) {
+      var products = JSON.parse(data);
+      res.render("index.ejs", { products : products });
+  });
+}
+else (mart,list[i].name.contains("롯데슈퍼")) {
+  fs.readFile("./lotteSuper.json", 'utf8', function (err, data) {
+      var products = JSON.parse(data);
+      res.render("index.ejs", { products : products });
+  });
+}
+else (mart,list[i].name.contains("롯데백화점")) {
+  fs.readFile("./lotteDepartment.json", 'utf8', function (err, data) {
+      var products = JSON.parse(data);
+      res.render("index.ejs", { products : products });
+  });
+}
+else if {
+  fs.readFile("./0806.json", 'utf8', function (err, data) {
+      var martAndProducts = JSON.parse(data);
+      res.render("index.ejs", { martAndProducts : martAndProducts });
+  });
+}
+*/
 
 
 /**
@@ -118,4 +146,56 @@ module.exports = router;
          // fs.writeFile('./martinfo.json', content, 'utf8', () => {
         //   console.log('Done');
         // });
- */
+  // for (var i = 0; i < 102; i++) {
+
+    //   const HOST = 'http://openapi.price.go.kr/openApiImpl/ProductPriceInfoService/getProductPriceInfoSvc.do?goodInspectDay=20210806&entpId=';
+    //   const SERVICE_KEY = '&ServiceKey=5ZDC6seJhCoIWl9KnCZsc0zxjP4SgXM4ZiEw1pLdbhrkF6pTmAwm90QVpwFyP4fHfxRoqWpczp69yYIAEjGb2Q==';
+    //   var num = mart.list[i].entpId;
+    //   var requestUrl = `${HOST}${num}${SERVICE_KEY}`;
+
+    //   request.get(requestUrl, (err, res, body)=> {
+    //     if (err) {
+    //         console.log(`err => ${err}`);
+    //     }
+    //     else {
+    //         if (res.statusCode == 200) {
+    //           var xmlToJson = convert.xml2json(body, {compact: true, spaces: 4});
+    //           var newJson = JSON.parse(xmlToJson);
+    //           var originalFile = fs.readFileSync('./productPriceInfo_0806.json', 'utf8');
+    //           var obj = JSON.parse(originalFile);
+    //           obj.list.push(newJson.response.result);
+    //           var json = JSON.stringify(obj);
+    //           fs.writeFileSync('./productPriceInfo_0806.json', json, 'utf8', () => {
+    //             console.log("done");
+    //           });
+    //         }
+    //     }
+    //   });
+    // }
+    for (var i = 0; i < 1019; i++) {
+
+      const HOST = 'http://openapi.price.go.kr/openApiImpl/ProductPriceInfoService/getProductPriceInfoSvc.do?goodInspectDay=20200313&entpId=';
+      const SERVICE_KEY = '&ServiceKey=5ZDC6seJhCoIWl9KnCZsc0zxjP4SgXM4ZiEw1pLdbhrkF6pTmAwm90QVpwFyP4fHfxRoqWpczp69yYIAEjGb2Q==';
+      var num = mart.list[i].entpId;
+      var requestUrl = `${HOST}${num}${SERVICE_KEY}`;
+
+      request.get(requestUrl, (err, res, body)=> {
+        if (err) {
+            console.log(`err => ${err}`);
+        }
+        else {
+            if (res.statusCode == 200) {
+              var xmlToJson = convert.xml2json(body, {compact: true, spaces: 4});
+              var newJson = JSON.parse(xmlToJson);
+              var originalFile = fs.readFileSync('./productPriceInfo_0806.json', 'utf8');
+              var obj = JSON.parse(originalFile);
+              obj.list.push(newJson.response.result);
+              var json = JSON.stringify(obj);
+              fs.writeFileSync('./productPriceInfo_0806.json', json, 'utf8', () => {
+                console.log("done");
+              });
+            }
+        }
+      });
+    }
+    */
